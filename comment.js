1acd21008acbe87f7f7c5842fd69d6498f71d335
JD <145668551+JDSP-23@users.noreply.github.com>
2024-03-04 23:05:10 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,69 +1,43 @@
 // Create web server
-// This is a simple web server to handle comments
-// The server will respond to any request with a simple HTML page
-// that lists all the comments that have been posted so far
-// and provides a form to enter a new comment.
+// 1. Create a web server
+// 2. Create a route for GET /comments
+// 3. Create a route for POST /comments
+// 4. Create a route for PUT /comments/:id
+// 5. Create a route for DELETE /comments/:id
 
-// Load the http module to create an http server.
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var querystring = require('querystring');
-var path = require('path');
-var comments = require('./comments');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  // Parse the request containing file name
-  var pathname = url.parse(request.url).pathname;
-  var query = url.parse(request.url).query;
-  var queryObj = querystring.parse(query);
-  var filename = "." + pathname;
-  var ext = path.extname(filename);
-  var type = '';
-  switch (ext) {
-    case '.html':
-      type = 'text/html';
-      break;
-    case '.js':
-      type = 'text/javascript';
-      break;
-    case '.css':
-      type = 'text/css';
-      break;
-    case '.json':
-      type = 'application/json';
-      break;
-    case '.png':
-      type = 'image/png';
-      break;
-    case '.jpg':
-      type = 'image/jpg';
-      break;
-    case '.wav':
-      type = 'audio/wav';
-      break;
-  }
-  if (filename === './') {
-    filename = './index.html';
-  }
-  if (queryObj['comment'] !== undefined) {
-    comments.addComment(queryObj['comment']);
-  }
-  if (pathname === '/getComments') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(comments.getComments()));
-  } else {
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        response.writeHead(404, { 'Content-Type': 'text/html' });
-        return response.end("404 Not Found");
-      }
-      response.writeHead(200, { 'Content-Type': type });
-      response.write(data);
-      return response.end();
-    });
-  }
+const app = express();
+app.use(bodyParser.json());
+
+let comments = [];
+
+app.get("/comments", (req, res) => {
+  res.json(comments);
 });
 
-// Listen on port 8080, IP defaults to
+app.post("/comments", (req, res) => {
+  const comment = req.body;
+  comment.id = comments.length + 1;
+  comments.push(comment);
+  res.status(201).json(comment);
+});
+
+app.put("/comments/:id", (req, res) => {
+  const id = req.params.id;
+  const comment = req.body;
+  comment.id = parseInt(id);
+  comments = comments.map((c) => (c.id === comment.id ? comment : c));
+  res.json(comment);
+});
+
+app.delete("/comments/:id", (req, res) => {
+  const id = req.params.id;
+  comments = comments.filter((c) => c.id !== parseInt(id));
+  res.status(204).end();
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
